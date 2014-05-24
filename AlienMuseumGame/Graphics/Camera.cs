f8dac45489a5d5c72cbad67ee8c232dd83394406
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-05-24 16:48:01 -0700

    Added Some Graphics Objects

---
+++ AlienMuseumGame/Graphics/Camera.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Graphics;
+
+namespace AlienMuseumGame{
+	  public class Camera {
+	  	 private SpriteBatch sb;
+		 private Vector2D pos;
+		 private Rectangle display;
+		 private Rectangle viewport;
+		 public void Draw(List<GraphicsObject> objects){
+		 }
+		 private Vector2D worldToScreen(Vector2D point){
+		 	 
+		 }
+	  
+	  }
+}
