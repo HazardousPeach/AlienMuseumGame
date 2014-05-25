61aad779ed22f71557e8746d8419ab7a0ee37113
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-05-24 23:41:13 -0700

    Fixed Position Bugs

--- AlienMuseumWindows/AlienMuseumWindows/Entity/Player.cs
+++ AlienMuseumWindows/AlienMuseumWindows/Entity/Player.cs
@@ -4,33 +4,36 @@ using Microsoft.Xna.Framework.Graphics;
 using TiledMax;
 
 namespace AlienMuseumGame{
-  public class Player : Entity{
+	public class Player : Entity, GraphicsObject{
     
     public static Texture2D PlayerTex {get;set;}
-    Vector2 pos;
 
     static Player(){
       Entity.RegisterEntity("player", MakePlayer);
     }
      
     
-    private Player(Vector2 position){
-      pos = position;
+    public Player(Vector2 position){
+			this.position = position;
     }
       public static Player MakePlayer(Vector2 position, Properties properties){
 	return new Player(position);
       }
     public void Update(){
-      float speed = 3.0f;
+			float speed = 7.0f;
       Vector2 dPos = Vector2.Zero;
       KeyboardState keystate = Keyboard.GetState();
 
-      if (keystate.IsKeyDown(Keys.A)) dPos += new Vector2(-speed, 0.0f);
-      if (keystate.IsKeyDown(Keys.S)) dPos += new Vector2(0.0f, speed);
-      if (keystate.IsKeyDown(Keys.D)) dPos += new Vector2(speed, 0.0f);
-      if (keystate.IsKeyDown(Keys.W)) dPos += new Vector2(0.0f, -speed);
+      if (keystate.IsKeyDown(Keys.A)) 
+				dPos += new Vector2(-speed, 0.0f);
+      if (keystate.IsKeyDown(Keys.S)) 
+				dPos += new Vector2(0.0f, speed);
+      if (keystate.IsKeyDown(Keys.D)) 
+				dPos += new Vector2(speed, 0.0f);
+      if (keystate.IsKeyDown(Keys.W)) 
+				dPos += new Vector2(0.0f, -speed);
 
-      pos += dPos;
+			this.position += dPos;
     }
     public override Texture2D getTexture(){ return PlayerTex;}
     public override Rectangle getFinalRectangle(){ return new Rectangle(0,0,PlayerTex.Width, PlayerTex.Height); }
