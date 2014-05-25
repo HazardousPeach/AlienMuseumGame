9734c13ea5768ce3f2852a6c675b758b244341ca
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-05-24 18:08:24 -0700

    Added Constructor To Camera

--- AlienMuseumGame/Graphics/Camera.cs
+++ AlienMuseumGame/Graphics/Camera.cs
@@ -4,9 +4,13 @@ using Microsoft.Xna.Graphics;
 namespace AlienMuseumGame{
 	  public class Camera {
 	  	 private SpriteBatch sb;
-		 private Vector2D pos;
 		 private Rectangle display;
 		 private Rectangle viewport;
+		 public Camera(SpriteBatch batch, Vector2 displayDimensions, Vector2 viewportDimensions){
+		 	sb = batch;
+			display = new Rectangle(0,0, displayDimensions.X, displayDimensions.Y);
+			viewport = new Rectangle(0,0, viewportDimensions.X, viewportDimensions.Y);
+		 }
 		 public void Draw(List<GraphicsObject> objects){
 		 	foreach(GraphicsObject obj in objects){
 			}
