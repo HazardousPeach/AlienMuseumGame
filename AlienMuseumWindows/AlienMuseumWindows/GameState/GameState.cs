5cf11807ab09e7a09bfcdf92ebe199e1ef8bed85
PirtualVineapple <VirtualPineapple@gmail.com>
2014-05-24 23:54:45 -0700

    GameState class

---
+++ AlienMuseumWindows/AlienMuseumWindows/GameState/GameState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlienMuseumWindows.GameState
+{
+    public abstract class GameState
+    {
+        public GameState()
+        {       
+        }
+        public abstract void Draw()
+        {
+
+        }
+        public abstract void Update()
+        {
+
+        }
+    }
+}
