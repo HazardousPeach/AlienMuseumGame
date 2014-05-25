64cfa7e33e36463d294be887408207bc1a71cbd7
Alex Sanchez-Stern <alex.sanchezstern@hotmail.com>
2014-05-24 20:51:55 -0700

    Added Callbacks and Entity Registration.

--- AlienMuseumWindows/AlienMuseumWindows/Entity/Entity.cs
+++ AlienMuseumWindows/AlienMuseumWindows/Entity/Entity.cs
@@ -4,8 +4,18 @@ using TiledMax;
 
 namespace AlienMuseumGame{
   public class Entity : GraphicsObject{
-    public static Entity MakeEnt(String typename, Vector2 Position, Properties properties){
+    public delegate Entity EntityFactory(Vector2 position, Properties properties);
+    private static Dictionary<String, EntityFactory> factories = new Dictionary<String, Entity>();
+    
+    public static Entity MakeEnt(String typename, Vector2 position, Properties properties){
+      if (factories.ContainsKey(typename)){
+	return factories[typename](position,properties);
+      }
       throw new ArgumentException("Unrecognized Entity Type: " + typename);
+    }
+    // A callback allowing you to register entities for creation.
+    public static void RegisterEntity(String name, EntityFactory template){
+      templates.add(name, template);
     }
   }
 }
